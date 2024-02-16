e93d0fb2f6201c1cde8eb34956d338b93717ce5c
Florian Gierlichs <florian.gierlichs@paymenttools.com>
2024-02-16 13:20:36 +0100

    Add Dashboard setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,19 @@
 import { getServerSession } from "next-auth";
 import LoginButton from "./components/LoginButton";
+import { Dashboard } from "./components/Dashboard";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-
+  console.log(session);
   return (
-    <div>
-      <h1>Workout Tracker</h1>
-
-      <div>
-        {session ? (
-          <p>
-            You are logged in as {session?.user?.email} and have access to the
-            application.
-          </p>
-        ) : (
-          <p>
-            You are not logged in and do not have access to the application.
-          </p>
-        )}
-        <LoginButton />
-      </div>
-    </div>
+    <>
+      {true ? (
+        <Dashboard />
+      ) : (
+        <p>You are not logged in and do not have access to the application.</p>
+      )}
+      <LoginButton />
+    </>
   );
 }
