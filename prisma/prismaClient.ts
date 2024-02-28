0ab83fb25c91c011df5700481b8b22b0f0d1ebe7
Florian Gierlichs <florian.gierlichs@paymenttools.com>
2024-02-28 13:49:35 +0100

    Add development prisma client workaround

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export default prisma;
+const myPrisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = myPrisma;
+
+export { myPrisma };
