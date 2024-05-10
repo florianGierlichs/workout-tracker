d410df38208acfb19fc80f7c6580dfb084ffe7e8
Florian Gierlichs <florian.gierlichs@paymenttools.com>
2024-05-10 20:37:37 +0200

    Change reps to selected range

--- app/components/AddExerciseSession.tsx
+++ app/components/AddExerciseSession.tsx
@@ -71,17 +71,10 @@ export const AddExerciseSession = ({
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               Reps
-              {/* TODO change to dropdown with rep ranges */}
             </label>
             <div className="mt-2">
-              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input
-                  type="text"
-                  id="reps"
-                  name="reps"
-                  placeholder="reps"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                />
+              <div className="rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <Select options={reps} form={{ id: "reps", name: "reps" }} />
               </div>
             </div>
           </div>
